62ae74dd7f6f1cf96b7d7acdcf0e9a71e05f8d64
Fabian Sabogal <fabian963@gmail.com>
2025-05-28 21:45:15 +0200

    Add global configuration for fetcher

---
+++ fetcher/configs/global.go
@@ -0,0 +1,33 @@
+package configs
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `env:"ENV" env-default:"development"`
+}
+
+var config *Config
+var configOnce sync.Once
+
+func GlobalConfigInstance() *Config {
+	configOnce.Do(func() {
+		var c Config
+
+		if err := cleanenv.ReadEnv(&c); err != nil {
+			log.Fatal("Failed to initialize global config: ", err)
+		}
+
+		config = &c
+	})
+
+	return config
+}
+
+func (sc *Config) Debug() bool {
+	return sc.Env == "development"
+}
