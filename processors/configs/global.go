34f774e6d6c8ecdb7db66f2b8442a2f241cd25ff
Fabian Sabogal <fabian963@gmail.com>
2025-05-29 11:52:39 +0200

    Add processor global config

---
+++ processors/configs/global.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env               string `env:"ENV" env-default:"development"`
+	RedisNewEventName string `env:"ENV" env-default:"new_event"`
+}
+
+var config *Config
+var configOnce sync.Once
+
+func GlobalConfigInstance() *Config {
+	configOnce.Do(func() {
+		var c Config
+
+		if err := cleanenv.ReadEnv(&c); err != nil {
+			log.Fatal("Failed to initialize global config: ", err)
+		}
+
+		config = &c
+	})
+
+	return config
+}
+
+func (sc *Config) Debug() bool {
+	return sc.Env == "development"
+}
