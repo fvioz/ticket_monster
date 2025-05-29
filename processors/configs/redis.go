dccd7f12f957b7eb7c5da42e17c94cab3dddeb96
Fabian Sabogal <fabian963@gmail.com>
2025-05-29 11:54:13 +0200

    Add processors redis library

---
+++ processors/configs/redis.go
@@ -0,0 +1,36 @@
+package configs
+
+import (
+	"log"
+	"net"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type RedisConfig struct {
+	Host                 string `env:"REDIS_HOST" env-default:"localhost"`
+	Port                 string `env:"REDIS_PORT" env-default:"6379"`
+	Username             string `env:"REDIS_USERNAME" env-default:""`
+	Password             string `env:"REDIS_PASSWORD" env-default:""`
+	Concurrency          uint   `env:"REDIS_CONCURRENCY" env-default:"10"`
+	MaxActive            int    `env:"REDIS_MAX_ACTIVE" env-default:"5"`
+	MaxIdle              int    `env:"REDIS_MAX_IDLE" env-default:"5"`
+	Wait                 bool   `env:"REDIS_WAIT" env-default:"true"`
+	ApplicationNamespace string `env:"REDIS_APPLICATION_NAMESPACE" env-default:"fetcher"`
+}
+
+func NewRedisConfig() *RedisConfig {
+	var config RedisConfig
+
+	if err := cleanenv.ReadEnv(&config); err != nil {
+		log.Fatal("Failed to initialize global config: ", err)
+	}
+
+	return &config
+}
+func (rc *RedisConfig) Address() string {
+	return net.JoinHostPort(
+		rc.Host,
+		rc.Port,
+	)
+}
