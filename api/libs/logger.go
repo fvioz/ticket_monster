5ecb9313d564ab13ad362492d01f97e0f78456e6
Fabian Sabogal <fabian963@gmail.com>
2025-05-29 17:36:28 +0200

    Add api logger lib

---
+++ api/libs/logger.go
@@ -0,0 +1,30 @@
+package libs
+
+import (
+	"api/configs"
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+var logerOnce sync.Once
+
+func LoggerInstance() *zap.Logger {
+	logerOnce.Do(func() {
+		var zapLogger *zap.Logger
+
+		c := configs.GlobalConfigInstance()
+
+		if c.Debug() {
+			zapLogger, _ = zap.NewDevelopment()
+			defer zapLogger.Sync()
+		} else {
+			zapLogger, _ = zap.NewProduction()
+			defer zapLogger.Sync()
+		}
+
+		logger = zapLogger
+	})
+	return logger
+}
