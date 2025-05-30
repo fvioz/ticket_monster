ef9d3d64f288e6db3a96ce9da07a3a16bf681a07
Fabian Sabogal <fabian963@gmail.com>
2025-05-30 11:10:08 +0200

    Remove custom validator and use inline validations

--- api/go.mod
+++ api/go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/brpaz/echozap v1.1.3
 	github.com/go-pg/pg/extra/pgotel/v10 v10.14.0
 	github.com/go-pg/pg/v10 v10.14.0
-	github.com/go-playground/validator/v10 v10.26.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/labstack/echo-contrib v0.17.4
 	github.com/labstack/echo/v4 v4.13.4
@@ -20,20 +19,16 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.1 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/go-pg/zerochecker v0.2.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
-	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- api/server/routes.go
+++ api/server/routes.go
@@ -6,20 +6,12 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/go-playground/validator/v10"
-
 	_ "api/docs"
 
 	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
 	"go.uber.org/zap"
-)
-
-type (
-	CustomValidator struct {
-		validator *validator.Validate
-	}
 )
 
 //	@title			Events API
@@ -32,38 +24,38 @@ func (s *Server) routes() {
 	logger := libs.LoggerInstance()
 	planHandler := handlers.NewPlansHandler()
 
-	s.echo.Validator = &CustomValidator{validator: validator.New()}
-
 	// @Summary Events Plans
 	// @Description get plans within a time range
 	// @Tags Plans
 	// @Accept json
 	// @Produce xml
 	// @Param starts_at query string true "Start time in format 2006-01-02T15:04:05"
-	// @Param end_at query string true "End time in format 2006-01-02T15:04:05"
+	// @Param ends_at query string true "End time in format 2006-01-02T15:04:05"
 	// @Success 200 {array} handlers.Plan
 	// @Failure 400 {object} echo.HTTPError "Invalid parameters"
 	// @Failure 500 {object} echo.HTTPError "Internal server error"
 	// @Router /v1/events/plans [get]
 	s.echo.GET("v1/events/plans", func(c echo.Context) error {
-		type Params struct {
-			start_at time.Time `params:"starts_at" validate:"required,datetime"`
-			end_at   time.Time `params:"end_at" validate:"required,datetime"`
+		startsAt, err := s.ParseTime(c.QueryParam("starts_at"))
+		if err != nil {
+			logger.Error("Failed to parse start time", zap.Error(err))
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid start time format, start_time parameter is required")
 		}
 
-		params := new(Params)
-		if err := c.Validate(params); err != nil {
-			return err
+		endsAt, err := s.ParseTime(c.QueryParam("ends_at"))
+		if err != nil {
+			logger.Error("Failed to parse end time", zap.Error(err))
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid end time format, end_time parameter is required")
 		}
 
-		plans, err := planHandler.GetPlansV1(params.start_at, params.end_at)
+		plans, err := planHandler.GetPlansV1(startsAt, endsAt)
 		if err != nil {
 			logger.Error("Failed to fetch plans", zap.Error(err))
 
-			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+			return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error")
 		}
 
-		return c.XMLPretty(http.StatusOK, plans, "  ")
+		return c.XMLPretty(http.StatusOK, &plans, "  ")
 	})
 
 	s.echo.GET("/swagger/*", echoSwagger.WrapHandler)
