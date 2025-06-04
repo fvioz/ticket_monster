ada1b1c7fa72838f67a14bab45ad5b45f0988b0c
Fabian Sabogal <fabian963@gmail.com>
2025-06-04 10:11:35 +0200

    Add queue limit and enabled to base_plans table

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,8 @@ CREATE DATABASE events;
 CREATE TABLE base_plans (
   id INT PRIMARY KEY,
   sell_mode VARCHAR (50),
+  queue_limit INT DEFAULT 1000,
+  queue_enabled BOOLEAN DEFAULT FALSE,
   title text
 );
 
@@ -19,6 +21,7 @@ CREATE TABLE plans (
   sold_out BOOLEAN
 );
 
+CREATE INDEX index_plan_id ON plans(base_plan_id);
 CREATE INDEX index_plans_dates ON plans(plan_start_date, plan_end_date);
 
 CREATE TABLE zones (
@@ -29,4 +32,13 @@ CREATE TABLE zones (
   name TEXT,
   sell_to TIMESTAMP,
   numbered BOOLEAN
+);
+
+CREATE INDEX index_plan_id ON zones(plan_id);
+
+CREATE TABLE clients_queue (
+  plan_id INT NOT NULL,
+  uuid INT PRIMARY KEY,
+  expiration_at TIMESTAMP SET DEFAULT (NOW() + INTERVAL '1 hour'),
+  PRIMARY KEY (plan_id, uuid)
 );
