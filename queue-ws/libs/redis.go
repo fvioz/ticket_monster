c33a1e0dd557bbc6ef400930202585ae1617f113
Fabian Sabogal <fabian963@gmail.com>
2025-06-04 15:24:29 +0200

    Add redis library to queue-ws app

---
+++ queue-ws/libs/redis.go
@@ -0,0 +1,49 @@
+package libs
+
+import (
+	"queue-ws/configs"
+	"sync"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+type RedisClient struct {
+	config *configs.RedisConfig
+	pool   *redis.Pool
+}
+
+var redisClient *RedisClient
+var redisClientOnce sync.Once
+
+func RedisInstance() *RedisClient {
+	redisClientOnce.Do(func() {
+		config := configs.NewRedisConfig()
+
+		var pool = &redis.Pool{
+			MaxActive: config.MaxActive,
+			MaxIdle:   config.MaxIdle,
+			Wait:      config.Wait,
+			Dial: func() (redis.Conn, error) {
+				return redis.Dial("tcp", config.Address(),
+					redis.DialUsername(config.Username),
+					redis.DialPassword(config.Password),
+				)
+			},
+		}
+
+		redisClient = &RedisClient{
+			config: config,
+			pool:   pool,
+		}
+	})
+
+	return redisClient
+}
+
+func (rc *RedisClient) Config() *configs.RedisConfig {
+	return rc.config
+}
+
+func (rc *RedisClient) Pool() *redis.Pool {
+	return rc.pool
+}
