69e210884967f74daf13e984a2c324a89ef0dbb7
Fabian Sabogal <fabian963@gmail.com>
2025-06-04 13:57:00 +0200

    Add QueueLimit and QueueEnabled ro the api

--- api/models/basePlan.go
+++ api/models/basePlan.go
@@ -1,8 +1,10 @@
 package models
 
 type BasePlan struct {
-	ID       int64  `json:"id" title:"id" description:"Unique identifier for the base plan"`
-	SellMode string `json:"sellMode" title:"sell_mode" description:"Mode of selling the plan,enum=online,enum=offline"`
-	Title    string `json:"title" title:"title" description:"Title of the base plan"`
-	Plans    []Plan `pg:"rel:has-many" json:"-" title:"plans" description:"List of plans associated with the base plan"`
+	ID           int64  `json:"id" title:"id" description:"Unique identifier for the base plan"`
+	SellMode     string `json:"sellMode" title:"sell_mode" description:"Mode of selling the plan,enum=online,enum=offline"`
+	QueueLimit   int64  `json:"queueLimit" title:"queue_limit" description:"Maximum number of clients allowed in the queue"`
+	QueueEnabled bool   `json:"queueEnabled" title:"queue_enabled" description:"Indicates if the queue is enabled for this plan"`
+	Title        string `json:"title" title:"title" description:"Title of the base plan"`
+	Plans        []Plan `pg:"rel:has-many" json:"-" title:"plans" description:"List of plans associated with the base plan"`
 }
